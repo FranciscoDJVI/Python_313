4833e4ea7b910f3b2e48494a9d281a6ca6688f85
Francisco Vanegas <vanegasfrancisco415@gmail.com>
2024-11-09 01:17:16 -0500

    Correion de decoradores 3

--- decoradores.py
+++ decoradores.py
@@ -4,11 +4,24 @@ def decorador(func):
         return func(*args, **kwargs)
     return wrapper
 
+
+def positvo(func):
+    def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if not result >0:
+                print("El resultado es un numero negativo")
+            else:
+                print("El resultado es un numero positivo")
+            return result
+    return wrapper
+
+@positvo
 @decorador
 def verficar_operacion():
     num= 3
-    num_2 = 3
-    square  = num * num_2
+    num_2 = 7
+    square  = num - num_2
     print(f"El resultado de la operacion es: {square}")
+    return square
 
 verficar_operacion()
